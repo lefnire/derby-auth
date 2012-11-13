14086e498175c9d84c97c81ab9cb836ebb2b87b8
Tyler Renelle <tylerrenelle@gmail.com>
2012-11-13 18:44:13 -0500

    begin pulling in template code for passport, replacing everyauth

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var everyauth = require('everyauth'),
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
     _  = require('lodash'),
-    ctx = {
-        model:undefined
-    }; // Keep context variables around for ./lib modules as pseudo-closure, they're set in middleware
+    strategyInstances = {},
+    model;
 
 /**
  * Creates middleware which provides authentication for DerbyJS
@@ -10,18 +10,18 @@ var everyauth = require('everyauth'),
  * @param {store} Racer store, used for configuring queries and accessControl
  * @param {conf} Authentication keys - see node_modules/everyauth/example.conf.js for format
  */
-module.exports.middleware = function(expressApp, store, conf) {
-    // Setup store
-    setupQueries(store);
-    setupAccessControl(store);
+module.exports.middleware = function(expressApp, store, strategies) {
+    // Setup queries & accessControl
+    require('./lib/store')(store);
 
-    // User passes in auth configuration keys originally. Provide defaults for ones not provided
-    conf = conf || {};
-    _.defaults(conf, require('everyauth/example/conf'));
-    setupEveryauth(conf);
+    // Initialize Passport!  Also use passport.session() middleware, to support
+    // persistent login sessions (recommended).
+    setupPassport(strategies);
+    expressApp.use(passport.initialize());
+    expressApp.use(passport.session());
 
-    expressApp.use(function(req, res, next) {
-        var model = ctx.model = req.getModel();
+    return function(req, res, next) {
+        model = req.getModel();
 
         // New User - They get to play around before creating a new account.
         var sess = model.session;
@@ -32,95 +32,181 @@ module.exports.middleware = function(expressApp, store, conf) {
             });
         }
         return next();
-    });
-    return everyauth.middleware();
+    };
 };
 
-setupQueries = function(store) {
-    store.query.expose('users', 'withId', function(id) {
-        return this.byId(id);
+/**
+ * Sets up static routes for Derby app. Normally this wouldn't be necessary, would just place this logic
+ * in middelware() setup. However, it breaks Derby routes - so we need this to call separately after expressApp
+ * hass been initialized
+ */
+module.exports.routes = function(expressApp) {
+    // POST /login
+    //   Use passport.authenticate() as route middleware to authenticate the
+    //   request.  If authentication fails, the user will be redirected back to the
+    //   login page.  Otherwise, the primary route function function will be called,
+    //   which, in this example, will redirect the user to the home page.
+    //
+    //   curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
+    expressApp.post('/login',
+        passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+        function(req, res) {
+            res.redirect('/');
+        });
+
+    // POST /login
+    //   This is an alternative implementation that uses a custom callback to
+    //   acheive the same functionality.
+    /*
+     app.post('/login', function(req, res, next) {
+     passport.authenticate('local', function(err, user, info) {
+     if (err) { return next(err) }
+     if (!user) {
+     req.flash('error', info.message);
+     return res.redirect('/login')
+     }
+     req.logIn(user, function(err) {
+     if (err) { return next(err); }
+     return res.redirect('/users/' + user.username);
+     });
+     })(req, res, next);
+     });
+     */
+
+    _.each(strategyInstances, function(strategy, name){
+        // GET /auth/facebook
+        //   Use passport.authenticate() as route middleware to authenticate the
+        //   request.  The first step in Facebook authentication will involve
+        //   redirecting the user to facebook.com.  After authorization, Facebook will
+        //   redirect the user back to this application at /auth/facebook/callback
+        expressApp.get('/auth/'+name,
+            passport.authenticate(name),
+            function(req, res){
+                // The request will be redirected to Facebook for authentication, so this
+                // function will not be called.
+            });
+
+        // GET /auth/facebook/callback
+        //   Use passport.authenticate() as route middleware to authenticate the
+        //   request.  If authentication fails, the user will be redirected back to the
+        //   login page.  Otherwise, the primary route function function will be called,
+        //   which, in this example, will redirect the user to the home page.
+        expressApp.get('/auth/' + name + '/callback',
+            passport.authenticate(name, { failureRedirect: '/login' }),
+            function(req, res) {
+                res.redirect('/');
+            });
     });
-    store.query.expose('users', 'withEveryauth', function(provider, id, password) {
-        if (password == null) {
-            password = null;
-        }
-        if (everyauth.debug) console.log({ withEveryauth: { provider: provider, id: id, password: password } });
-        if (password) {
-            return this.where("auth." + provider + ".id").equals(id).where("auth." + provider + ".password").equals(password);
+
+    expressApp.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    });
+    //    everyauth.everymodule.handleLogout(function(req, res) {
+    //        if (req.session.auth && req.session.auth.facebook) {
+    //            req.session.auth.facebook = void 0;
+    //        }
+    //        req.session.userId = void 0;
+    //        req.logout(); // The logout method is added for you by everyauth, too
+    //        return this.redirect(res, this.logoutRedirectPath());
+    //    });
+
+    // Simple route middleware to ensure user is authenticated.
+    //   Use this route middleware on any resource that needs to be protected.  If
+    //   the request is authenticated (typically via a persistent login session),
+    //   the request will proceed.  Otherwise, the user will be redirected to the
+    //   login page.
+    //    function ensureAuthenticated(req, res, next) {
+    //        if (req.isAuthenticated()) { return next(); }
+    //        res.redirect('/login')
+    //    }
+}
+
+function setupPassport(strategies) {
+    var users = [
+        { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+        , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+    ];
+
+    function findById(id, fn) {
+        var idx = id - 1;
+        if (users[idx]) {
+            fn(null, users[idx]);
         } else {
-            return this.where("auth." + provider + ".id").equals(id);
+            fn(new Error('User ' + id + ' does not exist'));
         }
-    });
-    return store.queryAccess('users', 'withEveryauth', function(methodArgs) {
-        var accept;
-        accept = arguments[arguments.length - 1];
-        return accept(true); // for now
-    });
-};
+    }
 
-setupAccessControl = function(store) {
-    store.accessControl = true;
-
-    //Callback(signatures(here(have(variable(length, eg(callback(captures..., next)))))));
-    //Is(using(arguments[n](the(correct(way(to(handle(typeof this !== "undefined" && this !== null))))))));
-
-    store.readPathAccess('users.*', function() { // captures, next) ->
-        var captures, next;
-        if (!(this.session && this.session.userId)) {
-            return; // https://github.com/codeparty/racer/issues/37
-        }
-        captures = arguments[0];
-        next = arguments[arguments.length - 1];
-        return next(captures === this.session.userId);
-    });
-    return store.writeAccess('*', 'users.*', function() { // captures, value, next) ->
-        var captures, next, pathArray;
-        if (!(this.session && this.session.userId)) {
-            return;
-        }
-        captures = arguments[0];
-        next = arguments[arguments.length - 1];
-        pathArray = captures.split('.');
-        return next(pathArray[0] === this.session.userId);
-    });
-};
-
-// Working on a hack to get password.js to play nicely with everyauth
-/*function setupExpress(expressApp) {
-    return expressApp.engine('html', (function() {
-        var cache;
-        cache = {};
-        return function(path, options, cb) {
-            var str;
-            try {
-                str = cache[path] || (cache[path] = fs.readFileSync(path, 'utf8'));
-                return cb(null, str);
-            } catch (err) {
-                return cb(err);
+    function findByUsername(username, fn) {
+        for (var i = 0, len = users.length; i < len; i++) {
+            var user = users[i];
+            if (user.username === username) {
+                return fn(null, user);
             }
-        };
-    })());
-};*/
+        }
+        return fn(null, null);
+    }
 
-function setupEveryauth(conf) {
-    everyauth.debug = true;
-    everyauth.everymodule.findUserById(function(id, callback) {
-        // will never be called, can't fetch user from database at this point on the server
-        // see https://github.com/codeparty/racer/issues/39. Handled in app/auth.coffee for now
-        return callback(null, null);
+    // Passport session setup.
+    //   To support persistent login sessions, Passport needs to be able to
+    //   serialize users into and deserialize users out of the session.  Typically,
+    //   this will be as simple as storing the user ID when serializing, and finding
+    //   the user by ID when deserializing.
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
     });
 
-    require('./lib/facebook')(everyauth, conf, ctx);
-    require('./lib/linkedin')(everyauth, conf, ctx);
-    require('./lib/github')(everyauth, conf, ctx);
-    require('./lib/password')(everyauth, conf, ctx);
+    passport.deserializeUser(function(id, done) {
+        findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
 
-    everyauth.everymodule.handleLogout(function(req, res) {
-        if (req.session.auth && req.session.auth.facebook) {
-            req.session.auth.facebook = void 0;
+    // Use the LocalStrategy within Passport.
+    //   Strategies in passport require a `verify` function, which accept
+    //   credentials (in this case, a username and password), and invoke a callback
+    //   with a user object.  In the real world, this would query a database;
+    //   however, in this example we are using a baked-in set of users.
+    passport.use(new LocalStrategy(
+        function(username, password, done) {
+            // asynchronous verification, for effect...
+            process.nextTick(function () {
+
+                // Find the user by username.  If there is no user with the given
+                // username, or the password is not correct, set the user to `false` to
+                // indicate failure and set a flash message.  Otherwise, return the
+                // authenticated `user`.
+                findByUsername(username, function(err, user) {
+                    if (err) { return done(err); }
+                    if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+                    if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+                    return done(null, user);
+                })
+            });
         }
-        req.session.userId = void 0;
-        req.logout(); // The logout method is added for you by everyauth, too
-        return this.redirect(res, this.logoutRedirectPath());
+    ));
+
+    _.each(strategies, function(arr){
+        var strategyClass = arr[0],
+            conf = arr[1];
+
+        // Use the FacebookStrategy within Passport.
+        //   Strategies in Passport require a `verify` function, which accept
+        //   credentials (in this case, an accessToken, refreshToken, and Facebook
+        //   profile), and invoke a callback with a user object.
+        var strategy = new strategyClass(conf, function(accessToken, refreshToken, profile, done) {
+                // asynchronous verification, for effect...
+                process.nextTick(function () {
+
+                    // To keep the example simple, the user's Facebook profile is returned to
+                    // represent the logged-in user.  In a typical application, you would want
+                    // to associate the Facebook account with a user record in your database,
+                    // and return that user instead.
+                    return done(null, profile);
+                });
+            }
+        )
+        passport.use(strategy);
+        strategyInstances[strategy.name] = strategy; // keep around for reference by routes()
     });
 };
