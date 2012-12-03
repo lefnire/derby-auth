b102b4001d6846e54f4712f6cbc4642120239c8d
Tyler Renelle <tylerrenelle@gmail.com>
2012-12-03 11:28:16 -0800

    registration client-side validation

--- components/register/index.js
+++ components/register/index.js
@@ -1,5 +1,53 @@
+var check = require('validator').check,
+    sanitize = require('validator').sanitize
+
 exports.init = function(model) {
 }
 
 exports.create = function(model, dom) {
+    model.on('set', 'username', function(username){
+        try {
+            check(username).isAlphanumeric();
+            model.set('errors.username', '');
+        } catch (err) {
+            model.set('errors.username', err.message);
+        }
+    });
+
+    model.on('set', 'email', function(email){
+        try {
+            check(email).isEmail();
+            model.set('errors.email', '');
+        } catch (err) {
+            model.set('errors.email', err.message);
+        }
+    });
+
+    model.on('set', 'emailConfirmation', function(emailConfirmation){
+        try {
+            check(emailConfirmation).equals(model.get('email'));
+            model.set('errors.emailConfirmation', '');
+        } catch (err) {
+            model.set('errors.emailConfirmation', err.message);
+        }
+    });
+
+    model.on('set', 'password', function(password){
+        try {
+            check(password).len(6);
+            model.set('errors.password', '');
+        } catch (err) {
+            model.set('errors.password', 'Password must be at least 6 characters');
+        }
+    });
+
+    model.on('set', 'errors.*', function(error){
+        var m = model.get(),
+            canSubmit = false;
+        if (!m.errors.username && !m.errors.email && !m.errors.emailConfirmation && !m.errors .password &&
+            !!m.username && !!m.email && !!m.emailConfirmation && !!m.password) {
+            canSubmit = true;
+        }
+        model.set('canSubmit', canSubmit);
+    })
 }
