25894733aa76a11602368201f9731637f9a73be4
Tyler Renelle <tylerrenelle@gmail.com>
2013-04-28 10:25:18 +0100

    re-order require statements for component-rendering fix

--- example/lib/auth/index.js
+++ example/lib/auth/index.js
@@ -1,12 +1,13 @@
-var derby = require('derby')
-    , app = derby.createApp(module)
-    , get = app.get
+var derby = require('derby');
 
+// initialize components (must come before derby.createApp)
 derby.use(require('derby-ui-boot'));
 derby.use(require('../../ui'));
 derby.use(require('../../../components'));
 
-get('/', function(page, model) {
+var app = derby.createApp(module);
+
+app.get('/', function(page, model) {
   model.query('users').withId(model.get('_userId')).subscribe(function(err, user) {
     model.ref('_user', user);
     page.render();
